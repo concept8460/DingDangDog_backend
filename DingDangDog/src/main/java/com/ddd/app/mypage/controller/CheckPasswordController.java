e48bacd87dcc19095d0bbe37fa5c3d2e32a8dd37
develop-omg0223 <lalalu0321@gmail.com>
2026-03-19 01:39:45 +0900

    260319 01:39 오명근 마이페이지 정보변경 진행중

--- DingDangDog/src/main/java/com/ddd/app/mypage/controller/CheckPasswordController.java
+++ DingDangDog/src/main/java/com/ddd/app/mypage/controller/CheckPasswordController.java
@@ -21,36 +21,25 @@ public class CheckPasswordController implements Execute {
 		System.out.println("===CheckPasswordController 실행===");
 
 		HttpSession session = request.getSession();
-		String inputPassword = request.getParameter("checkPassword");
-		System.out.println("1");
-		System.out.println("=======" + session);
-		if (session != null) {
-			System.out.println("받아옴?");
-		} else {
-			System.out.println("못받아옴");
-		}
 		UserDTO userDTO = new UserDTO();
-		System.out.println("2");
 		MypageDAO mypageDAO = new MypageDAO();
-		System.out.println("3");
 		Result result = new Result();
-		System.out.println("4");
-		int loginUserNumber =  Integer.parseInt((String) session.getAttribute("userNumber"));
-		System.out.println("5");
-		String loginUserType = (String) session.getAttribute("userType");
-		System.out.println("6");
 		String path = null;
 
-		System.out.println(loginUserNumber);
-		System.out.println(loginUserType);
-		System.out.println(inputPassword);
+		int loginUserNumber = (Integer) session.getAttribute("userNumber");
+		String loginUserType = (String) session.getAttribute("userType");
+		String inputPassword = request.getParameter("checkPassword");
+
+		System.out.println("회원번호 : " + loginUserNumber);
+		System.out.println("유저타입 : " + loginUserType);
+		System.out.println("입력한 비밀번호 :" + inputPassword);
 
 		userDTO.setUserNumber(loginUserNumber);
 		userDTO.setUserPassword(inputPassword);
 
-		int test = mypageDAO.checkPassword(userDTO);
+		int checkResult = mypageDAO.checkPassword(userDTO);
 
-		if (test == 1) {
+		if (checkResult == 1) {
 			if (loginUserType.equals("C")) {
 				path = request.getContextPath() + "/mypage/profileEditC.mp";
 			} else if (loginUserType.equals("S")) {
--- DingDangDog/src/main/java/com/ddd/app/mypage/dao/MypageDAO.java
+++ DingDangDog/src/main/java/com/ddd/app/mypage/dao/MypageDAO.java
@@ -1,5 +1,7 @@
 package com.ddd.app.mypage.dao;
 
+import java.util.Map;
+
 import org.apache.ibatis.session.SqlSession;
 
 import com.ddd.app.user.dto.UserDTO;
@@ -17,6 +19,30 @@ public class MypageDAO {
 	public int checkPassword(UserDTO userDTO) {
 		System.out.println("비밀번호 체크");
 		return sqlSession.selectOne("MPCInfo.checkPassword", userDTO);
+	}
+
+	// 정보변경페이지용 정보 조회
+	public UserDTO selectMyPageInfo(int UserNumber) {
+		System.out.println("마이페이지 정보 조회");
+		return sqlSession.selectOne("MPCInfo.selectMyPageInfo", UserNumber);
+	}
+
+	// 닉네임 체크
+	public boolean checkNickName(Map<String, Object> checkMap) {
+		System.out.println("닉네임 체크");
+		return (Integer) (sqlSession.selectOne("MPCInfo.checkNickName", checkMap)) < 1;
+	}
+
+	// 정보수정
+	public void updateMyPageInfo(Map<String, Object> updateMap) {
+		System.out.println("마이페이지 정보 수정");
+		sqlSession.update("MPCInfo.updateMyPageInfo", updateMap);
+	}
+
+	// 회원 탈퇴처리
+	public void updateToWithdraw(int userNumber) {
+		System.out.println("회원 탈퇴 처리 시작 " + userNumber + "번 회원");
+		sqlSession.update("MPCInfo.updateToWithdraw", userNumber);
 	}
 
 }
